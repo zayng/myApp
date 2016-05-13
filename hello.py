78dce845de04f40866ac0f87fb437fdf7f32e573
wb-zy184129 <orcexzy@163.com>
2016-05-13 18:28:01 +0800

    Minor changes

--- hello.py
+++ hello.py
@@ -60,9 +60,8 @@ def send_async_email(app, msg):
 def send_mail(to, subject, template, **kwargs):
     msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'],
                   recipients=[to])
-    # msg.body = render_template(template + '.txt', **kwargs)
-    # msg.html = render_template(template + '.html', **kwargs)
-    msg.body = 'body test'
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
     thr = Thread(target=send_async_email, args=[app, msg])
     thr.start()
     return thr
