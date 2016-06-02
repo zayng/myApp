c97913518f9a3db988b06ef7bced215dc1537b4b
wb-zy184129 <orcexzy@163.com>
2016-06-02 14:16:10 +0800

    Refactoring

--- app/main/views.py
+++ app/main/views.py
@@ -18,6 +18,15 @@ from ..models import User
 def index():
     form = NameForm()
     if form.validate_on_submit():
+        username = User.query.filter_by(username=form.name.data).first()
+        if username is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            session['known'] = False
+        else:
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data = ''
         return redirect(url_for('.index'))
     return render_template('index.html',
                            form=form, name=session.get('name'),
