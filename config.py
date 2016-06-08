1d13eab3f108d4f616dde9f7e4060787f82109d0
wb-zy184129 <orcexzy@163.com>
2016-06-08 18:17:18 +0800

    register username

--- config.py
+++ config.py
@@ -13,7 +13,13 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = True
-    FLASKY_MAIL_SENDER = 'Flask Admin <flasky@example.com>'
+    MAIL_SERVER = 'smtp.qq.com'
+    MAIL_PORT = 465
+    MAIL_USE_SSL = True
+    MAIL_USERNAME = '410105205@qq.com'
+    MAIL_PASSWORD = 'louqbesclhdvcaib'
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flask Admin <410105205@qq.com>'
     FLASK_ADMIN = os.environ.get('FLASK_ADMIN')
 
     @staticmethod
@@ -23,12 +29,6 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    MAIL_SERVER = 'smtp.163.com'
-    MAIL_PORT = 465
-    MAIL_USE_SSL = True
-
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
