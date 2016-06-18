c379e39e4a268d073ce0dbfc4d3386fd55e01275
sufy <orcexzy@163.com>
2016-06-19 01:02:29 +0800

    initial migrate 2016-06-19

--- app/models.py
+++ app/models.py
@@ -21,11 +21,33 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), unique=True)
-
+    default = db.Column(db.Boolean, default=False, index=True)
+    permissions = db.Column(db.Integer)
     users = db.relationship('User', backref='role', lazy='dynamic')
 
     def __repr__(self):
         return '<Role %r>' % self.name
+
+    @staticmethod
+    def insert_roles():
+        roles = {
+            'User': (Permission.FOLLOW|
+                     Permission.COMMENT|
+                     Permission.WRITE_ARTICLES, True
+                     ),
+            'Moderator': (Permission.FOLLOW|
+                          Permission.COMMENT|
+                          Permission.MODERATE_COMMENTS, False),
+            'Administrator': (0xff, False)
+        }
+        for r in roles:
+            role = Role.query.filter_by(name=r).first()
+            if role is None:
+                role = Role(name=r)
+            role.permissions = roles[r][0]
+            role.default = roles[r][1]
+            db.session.add(role)
+        db.session.commit()
 
 
 class User(UserMixin, db.Model):
@@ -65,11 +87,9 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
-
     def generate_reset_password_token(self, expiration=3600):
         s = Serializer(current_app.config['SECRET_KEY'], expiration)
         return s.dumps({'reset': self.id})
-
 
     def reset_password_confirm(self, token, new_password):
         s = Serializer(current_app.config['SECRET_KEY'])
@@ -83,6 +103,13 @@ class User(UserMixin, db.Model):
         db.session.add(self)
         return True
 
-
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Permission(object):
+    FOLLOW = 0x01
+    COMMENT = 0x02
+    WRITE_ARTICLES = 0x04
+    MODERATE_COMMENTS = 0x08
+    ADMINISTER = 0x80
