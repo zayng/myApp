f0126d3f9a2e209a6b5be150dd72c66c813bba24
wb-zy184129 <orcexzy@163.com>
2016-08-30 22:03:59 +0800

    restful

--- app/api_2_0/resources/users.py
+++ app/api_2_0/resources/users.py
@@ -4,10 +4,20 @@ Created on 2016/8/10
 
 @author: wb-zy184129
 """
-
+import random
 from flask_restful import fields, marshal,marshal_with, reqparse, Resource, url_for, current_app
 from ...models import User
 from ... import db
+
+
+def validation_null(value, name):
+    if value == "":
+        raise ValueError("{} cannot be empty".format(name, value))
+    else:
+        validate_user = User.query.filter_by(value).first()
+        if validate_user:
+            raise ValueError("{}:{},已注册".format(name,value))
+    return value
 
 
 user_list_parser = reqparse.RequestParser()
@@ -15,12 +25,12 @@ user_list_parser.add_argument('page', dest='page', type=int, location='args', de
 
 user_build_parser = user_list_parser.copy()
 user_build_parser.remove_argument('page')
-user_build_parser.add_argument('username', dest='username', type=str, required=True, location='json',
-                               help='用户名称必输.', trim=True, nullable=False)
-user_build_parser.add_argument('email', dest='email', type=str, required=True, location='json',
+user_build_parser.add_argument('username', dest='username', type=validation_null, required=True, location='json',
+                               trim=True, nullable=False)
+user_build_parser.add_argument('email', dest='email', type=validation_null, required=True, location='json',
                                help='邮箱必输.', trim=True, nullable=False)
-user_build_parser.add_argument('password', dest='password', type=str, required=True, location='json',
-                               help='密码必填.', trim=True, nullable=False)
+user_build_parser.add_argument('password', dest='password', type=validation_null, required=True, location='json',
+                               help='密码必填', trim=True, nullable=False)
 
 
 user_fields = {
@@ -31,7 +41,7 @@ user_fields = {
     'last_seen': fields.String,
     # 'posts': fields.String,
     # 'followed_posts': fields.String,
-    'post_count': fields.Integer,
+    'post_count': fields.Integer
 }
 
 user_page_fields = {
@@ -64,12 +74,20 @@ class UserListApi(Resource):
 
     def post(self):
         args = user_build_parser.parse_args(strict=True)
-        validate_user = User.query.filter_by(username=args.username).first()
-        if validate_user:
-            return {"message": "用户名已注册."}
-        validate_email = User.query.filter_by(email=args.email).first()
-        if validate_email:
-            return {"message": "邮箱已注册."}
+        # if args.username == "":
+        #     return {"message": "用户名不能为空."}
+        # else:
+        # validate_user = User.query.filter_by(username=args.username).first()
+        # if validate_user:
+        #     return {"message": "用户名已注册."}
+        # # if args.email == "":
+        # #     return {"message": "邮箱不能为空."}
+        # # else:
+        # validate_email = User.query.filter_by(email=args.email).first()
+        # if validate_email:
+        #     return {"message": "邮箱已注册."}
+        # # if args.password == "":
+        # #     return {"message": "密码不能为空."}
         user = User(username=args['username'], email=args['email'], password=args['password'])
         db.session.add(user)
         db.session.commit()
